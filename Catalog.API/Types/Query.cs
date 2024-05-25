3aa6b7a272da9606c2e9f15a2b4b38fdf63bc860
Karsten Pruehss <Karsten@Pruehss.com>
2024-05-25 15:48:54 +1000

    Add ProductType resolvers + Pagination for List queries

--- Catalog.API/Types/Query.cs
+++ Catalog.API/Types/Query.cs
@@ -2,7 +2,7 @@ namespace eShop.Catalog.Types;
 
 public class Query
 {
-    [UsePaging]
+    [UsePaging(DefaultPageSize = 1, MaxPageSize = 10)] // Override Global Pagination Settings
     [UseProjection]
     public IQueryable<Product> GetProducts(CatalogContext context)
         => context.Products;
@@ -12,6 +12,23 @@ public class Query
     public IQueryable<Product?> GetProductById(CatalogContext context, int id)
         => context.Products.Where(prd => prd.Id == id);
 
+    [UsePaging(DefaultPageSize = 1, MaxPageSize = 10)] // Override Global Pagination Settings
+    [UseProjection]
     public IQueryable<Brand> GetBrands
         (CatalogContext context) => context.Brands;
+
+    [UseFirstOrDefault]
+    [UseProjection]
+    public IQueryable<Brand?> GetBrandById(CatalogContext context, int id)
+        => context.Brands.Where(brand => brand.Id == id);
+
+    [UsePaging] // Override Global Pagination Settings
+    [UseProjection]
+    public IQueryable<ProductType> GetProductTypes(CatalogContext context)
+        => context.ProductTypes;
+
+    [UseFirstOrDefault]
+    [UseProjection]
+    public IQueryable<ProductType?> GetProductTypesById(CatalogContext context, int id)
+        => context.ProductTypes.Where(pt => pt.Id == id);
 }
