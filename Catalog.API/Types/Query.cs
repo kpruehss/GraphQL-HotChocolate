c7f541930f20344490ef0745ed7dcf7c89d39181
Karsten Pruehss <Karsten@Pruehss.com>
2024-05-26 09:09:22 +1000

    Implement default  sorting and filtering on requests

--- Catalog.API/Types/Query.cs
+++ Catalog.API/Types/Query.cs
@@ -1,3 +1,6 @@
+using HotChocolate.Data.Filters;
+using HotChocolate.Data.Sorting;
+
 namespace eShop.Catalog.Types;
 
 public class Query
@@ -39,8 +42,34 @@ public class Query
     [UseProjection]
     [UseFiltering]
     [UseSorting]
-    public IQueryable<Product> GetProducts(CatalogContext context)
-        => context.Products;
+    public IQueryable<Product> GetProducts(CatalogContext context, IFilterContext filterContext, ISortingContext sortingContext)
+    {
+        #region Provide default sorting/filtering for requests
+
+        // Disable automatic hands-off filtering/sorting
+        filterContext.Handled(false);
+        sortingContext.Handled(false);
+
+        IQueryable<Product> query = context.Products;
+
+        // Inspect if user provide a filter
+        if (!filterContext.IsDefined)
+        {
+            // Add a default filter, ex: by BrandId
+            // This can be used to create "Tenants" since any result will be appended to the base result
+            query = query.Where(f => f.BrandId == 1);
+        }
+
+        // Inspect if user provided sorting options
+        if (!sortingContext.IsDefined)
+        {
+            query = query.OrderBy(f => f.Brand!.Name).ThenByDescending(f => f.Price);
+        }
+
+        #endregion
+
+        return query;
+    }
 
     [UseFirstOrDefault]
     [UseProjection]
