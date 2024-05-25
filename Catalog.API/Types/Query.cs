48912028e0c5164dcb657527863888345e0ec8fe
Karsten Pruehss <Karsten@Pruehss.com>
2024-05-25 20:20:06 +1000

    Decorate queries with filters + add regions

--- Catalog.API/Types/Query.cs
+++ Catalog.API/Types/Query.cs
@@ -2,18 +2,27 @@ namespace eShop.Catalog.Types;
 
 public class Query
 {
-    [UsePaging(DefaultPageSize = 1, MaxPageSize = 10)] // Override Global Pagination Settings
+
+    #region ProductTypes
+
+    [UsePaging] // Override Global Pagination Settings
     [UseProjection]
-    public IQueryable<Product> GetProducts(CatalogContext context)
-        => context.Products;
+    [UseFiltering]
+    public IQueryable<ProductType> GetProductTypes(CatalogContext context)
+        => context.ProductTypes;
 
     [UseFirstOrDefault]
     [UseProjection]
-    public IQueryable<Product?> GetProductById(CatalogContext context, int id)
-        => context.Products.Where(prd => prd.Id == id);
+    public IQueryable<ProductType?> GetProductTypesById(CatalogContext context, int id)
+        => context.ProductTypes.Where(pt => pt.Id == id);
+
+    #endregion
+
+    #region Brands
 
     [UsePaging(DefaultPageSize = 1, MaxPageSize = 10)] // Override Global Pagination Settings
     [UseProjection]
+    [UseFiltering]
     public IQueryable<Brand> GetBrands
         (CatalogContext context) => context.Brands;
 
@@ -22,13 +31,21 @@ public class Query
     public IQueryable<Brand?> GetBrandById(CatalogContext context, int id)
         => context.Brands.Where(brand => brand.Id == id);
 
-    [UsePaging] // Override Global Pagination Settings
+    #endregion
+
+    #region Products
+
+    [UsePaging(DefaultPageSize = 1, MaxPageSize = 10)] // Override Global Pagination Settings
     [UseProjection]
-    public IQueryable<ProductType> GetProductTypes(CatalogContext context)
-        => context.ProductTypes;
+    [UseFiltering]
+    public IQueryable<Product> GetProducts(CatalogContext context)
+        => context.Products;
 
     [UseFirstOrDefault]
     [UseProjection]
-    public IQueryable<ProductType?> GetProductTypesById(CatalogContext context, int id)
-        => context.ProductTypes.Where(pt => pt.Id == id);
+    public IQueryable<Product?> GetProductById(CatalogContext context, int id)
+        => context.Products.Where(prd => prd.Id == id);
+
+    #endregion
+
 }
