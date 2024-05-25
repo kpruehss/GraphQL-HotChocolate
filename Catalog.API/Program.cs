a452dda9128a90d82a163705ea22a4fd4aa89706
Karsten Pruehss <Karsten@Pruehss.com>
2024-05-25 20:19:32 +1000

    Add filtering + custom string filter type

--- Catalog.API/Program.cs
+++ Catalog.API/Program.cs
@@ -1,3 +1,4 @@
+using HotChocolate.Data.Filters;
 using HotChocolate.Types.Pagination;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +14,6 @@ builder.Services
 builder.Services
     .AddGraphQLServer()
     .AddQueryType<Query>()
-    .AddProjections()
     .SetPagingOptions(new PagingOptions
     {
         // Global Pagination Defaults
@@ -21,6 +21,12 @@ builder.Services
         MaxPageSize = 5,
         AllowBackwardPagination = false,
         RequirePagingBoundaries = true,
+    })
+    .AddProjections()
+    .AddFiltering(c =>
+    {
+        c.AddDefaults()
+            .BindRuntimeType<string, CustomStringOperationFilterInputType>();
     });
 
 var app = builder.Build();
@@ -28,3 +34,15 @@ var app = builder.Build();
 app.MapGraphQL();
 
 app.RunWithGraphQLCommands(args);
+
+public class CustomStringOperationFilterInputType : StringOperationFilterInputType
+{
+    protected override void Configure(IFilterInputTypeDescriptor descriptor)
+    {
+        descriptor.Operation(DefaultFilterOperations.Equals).Type<StringType>();
+        descriptor.Operation(DefaultFilterOperations.Contains).Type<StringType>();
+        descriptor.Operation(DefaultFilterOperations.In).Type<ListType<StringType>>();
+        descriptor.Operation(DefaultFilterOperations.StartsWith).Type<StringType>();
+        descriptor.Operation(DefaultFilterOperations.EndsWith).Type<StringType>();
+    }
+}
