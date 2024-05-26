e3c0f0ee7081decfe3cc94fffcc02958dde41966
Karsten Pruehss <Karsten@Pruehss.com>
2024-05-26 12:35:03 +1000

    Refactor ProductQueries into service layer

---
+++ Catalog.API/Service/ProductService.cs
@@ -0,0 +1,13 @@
+namespace eShop.Catalog.Service;
+
+[QueryType]
+public sealed class ProductService(CatalogContext context)
+{
+    public async Task<IReadOnlyList<Product>> GetProductsAsync(CancellationToken cancellationToken = default) =>
+        await context.Products.ToListAsync(cancellationToken);
+
+    public async Task<Product?> GetProductByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        return await context.Products.FirstOrDefaultAsync(t => t.Id == id, cancellationToken);
+    }
+}
--- Catalog.API/Types/ProductQueries.cs
+++ Catalog.API/Types/ProductQueries.cs
@@ -1,48 +1,12 @@
-using HotChocolate.Data.Filters;
-using HotChocolate.Data.Sorting;
+using eShop.Catalog.Service;
 
 namespace eShop.Catalog.Types;
 
-[QueryType]
 public static class ProductQueries
 {
+    public static async Task<IReadOnlyList<Product>> GetProductsAsync(ProductService productService, CancellationToken cancellationToken = default)
+        => await productService.GetProductsAsync(cancellationToken);
 
-
-    [UsePaging] // Override Global Pagination Settings
-    [UseProjection]
-    [UseFiltering]
-    [UseSorting]
-    public static IQueryable<Product> GetProducts(CatalogContext context, IFilterContext filterContext, ISortingContext sortingContext)
-    {
-        #region Provide default sorting/filtering for requests
-
-        // Disable automatic hands-off filtering/sorting
-        filterContext.Handled(false);
-        sortingContext.Handled(false);
-
-        IQueryable<Product> query = context.Products;
-
-        // Inspect if user provide a filter
-        if (!filterContext.IsDefined)
-        {
-            // Add a default filter, ex: by BrandId
-            // This can be used to create "Tenants" since any result will be appended to the base result
-            query = query.Where(f => f.BrandId == 1);
-        }
-
-        // Inspect if user provided sorting options
-        if (!sortingContext.IsDefined)
-        {
-            query = query.OrderBy(f => f.Brand!.Name).ThenByDescending(f => f.Price);
-        }
-
-        #endregion
-
-        return query;
-    }
-
-    [UseFirstOrDefault]
-    [UseProjection]
-    public static IQueryable<Product?> GetProductById(CatalogContext context, int id)
-        => context.Products.Where(prd => prd.Id == id);
+    public static async Task<Product?> GetProductByIdAsync(int id, ProductService productService, CancellationToken cancellationToken = default)
+        => await productService.GetProductByIdAsync(id, cancellationToken);
 }
